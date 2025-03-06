9430d1f907d95af8f8b50863c1d676e3c378655f
Dnduni <donadio.1795337@studenti.uniroma1.it>
2025-03-06 14:34:49 +0100

    aggiunti grafici, migliorati gli script

--- linearity.py
+++ linearity.py
@@ -5,6 +5,10 @@ times = [10,50,100,200,500,1000,10000,20000,60000,100000,200000]
 ambient = [0,0,0,0,5,9,97,193,254,254,254]
 darkbox = [0,0,0,0,0,0,8,16,50,85,170]
 
-plt.plot(times,ambient)
-plt.plot(times,darkbox)
+fig = plt.plot(times,ambient, label = 'ambient')
+fig  = plt.plot(times,darkbox, label = 'dark')
+plt.legend()
+plt.xlabel("Exposure time (ms)")
+plt.ylabel("Pixel value")
+plt.savefig("Linearity.png")
 plt.show()
